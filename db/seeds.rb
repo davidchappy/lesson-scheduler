a368b0a4ef0b727a599c33f795cf1b4ef2d6b996
David Chapman <davidchappy@gmail.com>
2017-02-13 19:12:10 -0300

    Add DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+piano = Instrument.create(name: 'Piano')
+
+# admin = Admin.create(first_name: 'DMS', last_name: 'Admin', 
+#   email: 'admin@dmusicstudios.com', 
+#   password: 'password', password_confirmation: 'password')
+
+teacher = Teacher.create(first_name: 'Piano', last_name: 'Teacher', 
+  instrument_id: piano.id)
+
+family = Family.create(first_name: 'Lovely', last_name: 'Family', 
+  email: 'family@example.com',
+  password: 'password', password_confirmation: 'password')
+
+student = Student.create(first_name: 'First', last_name: 'Student', 
+  family_id:family.id,  instrument_id: piano.id)

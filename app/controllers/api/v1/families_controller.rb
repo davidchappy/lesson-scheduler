770879b4729a02e61b35785ede8fc22b3cf7d0ac
David Chapman <davidchappy@gmail.com>
2017-02-15 22:05:06 -0300

    Add form sorting

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::FamiliesController < Api::V1::BaseController
   def index
     if current_user.type == 'Family'
       @family = Family.find(current_user.id)
-      @forms = Form.where(family_id: @family.id)
+      forms = Form.where(family_id: @family.id)
+      @forms = forms.sort_by { |form| form.created_at }
       respond_with [@family, @forms]
     else
       respond_with Family.all

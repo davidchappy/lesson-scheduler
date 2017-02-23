c74d99f51560204652c0b5490def60ecbdfb9ba4
David Chapman <davidchappy@gmail.com>
2017-02-23 10:34:09 -0300

    Remove family_id on students from migrations

--- db/migrate/20170213220752_create_students.rb
+++ db/migrate/20170213220752_create_students.rb
@@ -3,7 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.integer :instrument_id
       t.string  :student_name
-      t.integer :family_id
       t.integer :teacher_id
       t.date    :start_date
       t.date    :end_date

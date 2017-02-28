bcf9751da603773020df2a94009daecf3c2457f9
David Chapman <davidchappy@gmail.com>
2017-02-28 15:26:24 -0300

    Refactor week model test

--- spec/models/week_spec.rb
+++ spec/models/week_spec.rb
@@ -5,25 +5,24 @@ RSpec.describe Week, :type => :model do
   let(:family)     { create(:family) }
   let(:instrument) { create(:instrument) }
   let(:teacher)    { create(:teacher) }
+  let(:student)    { create(:student) }
   let(:form)       { family.find_or_create_current_form }
-  let!(:student) do
-    student = form.students.create( student_name:   "Susan", 
-                                    instrument_id:  instrument.id,
-                                    teacher_id:     teacher.id,
-                                    start_date:     Date.new(2017, 6, 5),
-                                    end_date:       Date.new(2017, 9, 1))
-    student.save
-    return student       
+  let!(:lesson_period) do
+    lesson_period = form.lesson_periods.create( student_id:student.id,
+                                                instrument_id: instrument.id,
+                                                teacher_id: teacher.id )
+    lesson_period.save
+    return lesson_period       
   end
 
-  it "belongs to a student" do
-    week = student.weeks.last
-    expect(week).to respond_to(:student) 
+  it "belongs to a lesson period" do
+    week = lesson_period.weeks.last
+    expect(week).to respond_to(:lesson_period) 
   end
 
   it "is destroyed when its student is destroyed" do
-    week = student.weeks.last
-    student.destroy
+    week = lesson_period.weeks.last
+    lesson_period.destroy
     expect(week.reload).to_not be_valid
   end
 

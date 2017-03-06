ccd33a4f27fe96acbf65ef6d84db1d1e50d4ea01
David Chapman <davidchappy@gmail.com>
2017-03-06 10:58:01 -0300

    Add App Controller

---
+++ app/controllers/api/v1/app_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::AppController < Api::V1::BaseController
+
+  def index
+    @family = Family.find(current_user.id)
+    @students = @family.students
+    @form = @family.find_or_create_current_form 
+    @lesson_periods = @form.sorted_lesson_periods
+
+    response =  family: @family, form: @form, 
+                lesson_periods: @lesson_periods, students: @students
+    respond_with response, json: response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
-      resources :lesson_periods, only: [:index, :show, :create, :destroy, :update]
+      resources :app, only: [:index]
       resources :families, only: [:index, :submit]
-      resources :weeks, only: [:index, :update]
       resources :forms, only: [:index, :show, :update]
+      resources :lesson_periods, only: [:index, :show, :create, :destroy, :update]
+      resources :weeks, only: [:index, :update]
       resources :instruments, only: [:index, :create, :destroy]
       resources :teachers, only: [:index, :create, :destroy, :update]
     end

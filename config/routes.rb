42d7dda5b6697ce3290f7f04d5699d70746394fe
David Chapman <davidchappy@gmail.com>
2017-03-07 16:27:31 -0300

    Fix weeks tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :app, only: [:index]
       resources :forms, only: [:index, :show, :update]
       resources :lesson_periods, only: [:index, :show, :create, :destroy, :update]
-      resources :weeks, only: [:index, :update]
+      resources :weeks, only: [:update]
       resources :instruments, only: [:index, :create, :destroy]
       resources :teachers, only: [:index, :create, :destroy, :update]
     end
--- spec/controllers/api/v1/weeks_controller_spec.rb
+++ spec/controllers/api/v1/weeks_controller_spec.rb
@@ -5,9 +5,6 @@ RSpec.describe Api::V1::WeeksController, :type => :controller do
   
   context "anonymous user" do
     it "should be redirected to signin" do
-      get :index
-      expect(subject).to redirect_to(new_user_session_url)
-
       get :update, params: { id: 1 }
       expect(subject).to redirect_to(new_user_session_url)
     end
@@ -37,19 +34,8 @@ RSpec.describe Api::V1::WeeksController, :type => :controller do
       expect(subject.current_user).to eq(family)
 
       sign_out family
-      get :index, format: :json, params: { id: 1 }
+      get :update, format: :json, params: { id: 1 }
       expect(response.body).to include("You need to sign in or sign up before continuing.")
-    end
-
-    describe "index" do
-      it "returns a sorted array of weeks for the given student" do
-        get :index, format: :json, params: { lesson_period_id: lesson_period.id }
-
-        original_weeks = lesson_period.weeks.sort_by { |week| week.start_date }
-        response_weeks = response_body
-
-        expect(response_body).to eq( JSON.parse(response_weeks.to_json) )
-      end
     end
 
     describe "update" do

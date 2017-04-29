71820626dae06b6d20a2d216f8cdd93b7434dd7a
David Chapman <davidchappy@gmail.com>
2017-04-29 20:35:20 -0300

    2nd attempt to fix migrations for production

--- db/migrate/20170221172150_add_unavailable_dates_to_teachers.rb
+++ db/migrate/20170221172150_add_unavailable_dates_to_teachers.rb
@@ -1,5 +1,5 @@
 class AddUnavailableDatesToTeachers < ActiveRecord::Migration[5.0]
   def change
-    add_column :teachers, :unavailable_weeks, :text
+    add_column :teachers, :unavailable_dates, :text
   end
 end

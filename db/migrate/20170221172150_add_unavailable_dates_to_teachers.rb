89971f54c285c6fd95ac610017967bac948525fe
David Chapman <davidchappy@gmail.com>
2017-04-29 20:31:22 -0300

    Rename migration for production

--- db/migrate/20170221172150_add_unavailable_dates_to_teachers.rb
+++ db/migrate/20170221172150_add_unavailable_dates_to_teachers.rb
@@ -1,4 +1,4 @@
-class AddunavailableWeeksToTeachers < ActiveRecord::Migration[5.0]
+class AddUnavailableDatesToTeachers < ActiveRecord::Migration[5.0]
   def change
     add_column :teachers, :unavailable_weeks, :text
   end

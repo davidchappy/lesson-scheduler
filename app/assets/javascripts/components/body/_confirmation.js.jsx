0ae435bda5787af4b5847857e5111059ed465477
David Chapman <davidchappy@gmail.com>
2017-05-02 10:45:18 -0300

    Refactor Confirmation and Payment plan

--- app/assets/javascripts/components/body/_confirmation.js.jsx
+++ app/assets/javascripts/components/body/_confirmation.js.jsx
@@ -1,28 +1,15 @@
 var Confirmation = React.createClass({
-  getInitialState() {
-    return { totalDiscount: 0, totalOwed: 0, possibleDiscount: 0 }
-  },
-  componentDidMount() {
-    this.updatePricing(this.props.lessonCount, this.props.lessonPeriods);
-  },
-  updatePricing(count, lessonPeriods) {
-    var settings = this.props.appSettings;
-    var pricing = Pricer.getPricing(lessonPeriods, this.props.allWeeks,
-                                          settings.baseLessonLength.value,
-                                          settings.thirtyMinRate.value);
-    this.setState({
-      totalDiscount: pricing.discount,
-      totalOwed: pricing.totalOwed,
-      possibleDiscount: pricing.possibleDiscount
-    })  
-  },
+
   render() {
+    var total = Pricer.monetize(this.props.pricingData.currentPricing.totalOwed);
+    var payment = Pricer.monetize(this.props.pricingData.currentPricing.payment);
+
     return ( 
       <div className='confirmation-page col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3'> 
         <button className='btn btn-default' onClick={this.props.toggleConfirming}>Back</button>
         <div className='confirmation-text'>
-          <p>Here's a quick breakdown of your payments for this summer.</p>
-          <PaymentPlan total={this.state.totalOwed} payment={"payment"} />
+          <p>Here is a quick breakdown of your payments for this summer.</p>
+          <PaymentPlan  total={total} payment={payment} />
           <p>If everything looks good, just click the green button!</p>
         </div>
         <div>
@@ -32,4 +19,5 @@ var Confirmation = React.createClass({
       </div>
     )
   }
+  
 });

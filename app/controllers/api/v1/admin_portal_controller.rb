9df6ab09c0e0b0b9be03887620c787979aa05579
David Chapman <davidchappy@gmail.com>
2017-06-13 17:14:31 -0400

    In Admin view, order families by last name

--- app/controllers/api/v1/admin_portal_controller.rb
+++ app/controllers/api/v1/admin_portal_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AdminPortalController < Api::V1::BaseController
 	def index
 		instruments = JSON.parse(Instrument.all.order(:created_at).to_json(include: :teachers))
 		teachers = JSON.parse(Teacher.all.order(:created_at).to_json(include: [:instruments, :unavailable_weeks]))
-		families = JSON.parse(Family.all.order(:created_at).to_json(include: [:lesson_periods, :setting_profiles, :forms]))
+		families = JSON.parse(Family.all.order(:last_name).to_json(include: [:lesson_periods, :setting_profiles, :forms]))
 		students = Student.all.order(:created_at)
 
 		# check summerWeeks setting exists for current year or create it

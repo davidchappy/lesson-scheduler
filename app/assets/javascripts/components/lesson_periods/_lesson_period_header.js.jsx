d9e982ca9b5e1f1d2d9980ec6afc6ced8c6a1833
David Chapman <davidchappy@gmail.com>
2018-06-03 22:21:25 -0500

    Hot fix - null teacher in lesson period

--- app/assets/javascripts/components/lesson_periods/_lesson_period_header.js.jsx
+++ app/assets/javascripts/components/lesson_periods/_lesson_period_header.js.jsx
@@ -4,11 +4,11 @@ var LessonPeriodHeader = React.createClass({
       <div>
         <h3>{this.props.student.name}</h3>
         <p className="instrument">{this.props.instrument.name}</p>
-        <p className="teacher">{this.props.teacher.first_name} {this.props.teacher.last_name}</p>
+        <p className="teacher">{this.props.teacher && this.props.teacher.first_name} {this.props.teacher && this.props.teacher.last_name}</p>
         <p className="lesson-count"><strong>{this.props.lessonPeriod.lesson_count}</strong> Lessons</p>
         <div className="lesson-period-hover-menu">
-          <span className="edit-lesson-period glyphicon glyphicon-pencil" title="Edit" onClick={this.props.toggleEditing}></span> 
-          <span className="delete-lesson-period glyphicon glyphicon-remove" title="Delete" onClick={this.props.handleDelete}></span> 
+          <span className="edit-lesson-period glyphicon glyphicon-pencil" title="Edit" onClick={this.props.toggleEditing}></span>
+          <span className="delete-lesson-period glyphicon glyphicon-remove" title="Delete" onClick={this.props.handleDelete}></span>
         </div>
       </div>
     )

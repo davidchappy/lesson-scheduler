58c0ea7835280503b3909f9558c07e0f6f8f506e
David Chapman <davidchappy@gmail.com>
2019-05-20 13:52:48 -0500

    Add production host value for Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dms100-summer-form_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'summer.dmusicstudios.com' }
+
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

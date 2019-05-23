71ca0717fb13dd1df6ffc125715222b2dbd8f2cc
David Chapman <davidchappy@gmail.com>
2019-05-23 16:24:09 -0500

    Remove unavailable weeks from other years

--- app/controllers/api/v1/app_controller.rb
+++ app/controllers/api/v1/app_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::AppController < Api::V1::BaseController
   include ApplicationHelper
-  before_action :eliminate_old_profiles
+  before_action :eliminate_old_profiles, :eliminate_old_unavailable_weeks
 
   def index
     @family = nil
     if current_user.type === 'Admin' && params["family_id"]
       @family = Family.find(params["family_id"])
     else
-      @family = Family.find(current_user.id)      
+      @family = Family.find(current_user.id)
     end
     @instruments = JSON.parse(Instrument.all.order(:created_at).to_json(include: :teachers))
     @teachers = JSON.parse(Teacher.all.order(:created_at).to_json(include: [:instruments, :unavailable_weeks]))
     @students = @family.students
-    @form = @family.find_or_create_current_form 
+    @form = @family.find_or_create_current_form
     @lesson_periods = @form.lesson_periods.order(:created_at)
     @weeks = LessonPeriod.get_weeks_as_hash(@lesson_periods)
     @app_settings = AppSetting.all.index_by(&:key)
     @content_entries = ContentEntry.all.index_by(&:key)
     @all_setting_profiles = SettingProfile.all.order(:created_at)
-    
+
     # replace general app settings with custom settings
     setting_profiles = @family.setting_profiles.all.order(:created_at)
     setting_profiles.each do |profile|
@@ -33,8 +33,8 @@ class Api::V1::AppController < Api::V1::BaseController
     @family.save
     @family = JSON.parse(@family.to_json(include: :setting_profiles))
 
-    respond_with  instruments: @instruments, teachers: @teachers, 
-                  family: @family, students: @students, form: @form, 
+    respond_with  instruments: @instruments, teachers: @teachers,
+                  family: @family, students: @students, form: @form,
                   lesson_periods: @lesson_periods, weeks: @weeks,
                   messages: flash_messages, app_settings: @app_settings,
                   content_entries: @content_entries, setting_profiles: @all_setting_profiles
@@ -58,6 +58,14 @@ class Api::V1::AppController < Api::V1::BaseController
       profiles = SettingProfile.all
       profiles.each do |profile|
         profile.families.destroy_all if profile.expiration < Date.today
+      end
+    end
+
+    def eliminate_old_unavailable_weeks
+      unavailable_weeks = UnavailableWeek.all
+
+      unavailable_weeks.each do |week|
+        week.destroy if week.start_date.year != Date.today.year
       end
     end
 
